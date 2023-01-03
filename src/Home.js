bd2839be5f3a7bf5769541dd068b2c1480836e6e
Noor Mohammad <noormohammad@AAOMAN.COM>
2023-01-03 15:42:46 +0400

    amazonclone updated

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "./components/banner/Banner";
+import Header from "./components/header/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/assets/index.js
@@ -0,0 +1,15 @@
+import logo from "./logo.png";
+import bannerImgOne from "./banner/bannerImgOne.jpg";
+import bannerImgTwo from "./banner/bannerImgTwo.jpg";
+import bannerImgThree from "./banner/bannerImgThree.jpg";
+import bannerImgFour from "./banner/bannerImgFour.jpg";
+import bannerImgFive from "./banner/bannerImgFive.jpg";
+
+export {
+  logo,
+  bannerImgOne,
+  bannerImgTwo,
+  bannerImgThree,
+  bannerImgFour,
+  bannerImgFive,
+};
